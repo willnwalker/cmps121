37a5c3baf74cc824a89160a704e7dd02a366a211
William Walker <wnwalker@ucsc.edu>
2019-04-16 00:35:41 -0700

    Hobby Fragment and Buttons
    --Added Hobby Fragment
    --Added Button logic to exit app on click

--- asg1/app/src/main/java/xyz/willnwalker/asg1/MainActivity.kt
+++ asg1/app/src/main/java/xyz/willnwalker/asg1/MainActivity.kt
@@ -3,10 +3,19 @@ package xyz.willnwalker.asg1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainActivityListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun finish() {
+        finish()
+    }
+
+}
+
+interface MainActivityListener{
+    fun finish()
 }
--- asg1/app/src/main/java/xyz/willnwalker/asg1/MainFragment.kt
+++ asg1/app/src/main/java/xyz/willnwalker/asg1/MainFragment.kt
@@ -1,16 +1,21 @@
 package xyz.willnwalker.asg1
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
+
+    private lateinit var mainActivityListener: MainActivityListener
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -18,5 +23,16 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        saveExitButton.setOnClickListener {
+            mainActivityListener.finish()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivityListener = context as MainActivityListener
+    }
 
 }
