1abc3629507abb43a10ec3bd2f847ca1dc55cd12
William Walker <wnwalker@ucsc.edu>
2019-05-09 11:15:46 -0700

    Assignment 2 Refactor
    --Refactored Assignment 2 to use ViewModel paradigm
    --Added navigation component and set MainFragment as starting destination

--- asg2/app/src/main/java/xyz/willnwalker/assignment2/MainActivity.kt
+++ asg2/app/src/main/java/xyz/willnwalker/assignment2/MainActivity.kt
@@ -2,11 +2,13 @@ package xyz.willnwalker.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.willnwalker.assignment2.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
     }
+
 }
---
+++ asg2/app/src/main/java/xyz/willnwalker/assignment2/ui/main/MainFragment.kt
@@ -0,0 +1,34 @@
+package xyz.willnwalker.assignment2.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import xyz.willnwalker.assignment2.R
+
+class MainFragment : Fragment() {
+
+    val viewModel by viewModels<MainViewModel>()
+    private lateinit var navController: NavController
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+    }
+
+}
