90dca2305ee9247214c3e3606ac9907e2dbaeb3a
William Walker <wnwalker@ucsc.edu>
2019-05-20 23:32:05 -0700

    Started Assignment 3

---
+++ asg3/app/src/main/java/xyz/willnwalker/asg3/MainActivity.kt
@@ -0,0 +1,12 @@
+package xyz.willnwalker.asg3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
