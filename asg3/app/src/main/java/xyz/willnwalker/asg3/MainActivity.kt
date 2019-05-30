9e88a13cf993bcc78f1a95f0cb17dac081714c74
William Walker <wnwalker@ucsc.edu>
2019-05-29 17:19:45 -0700

    Service initialization
    --Added click listeners for buttons
    --Added code to start and stop service at appropriate times
    --Upgraded Gradle Version

--- asg3/app/src/main/java/xyz/willnwalker/asg3/MainActivity.kt
+++ asg3/app/src/main/java/xyz/willnwalker/asg3/MainActivity.kt
@@ -1,12 +1,49 @@
 package xyz.willnwalker.asg3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var serviceIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clearButton.setOnClickListener(this)
+        exitButton.setOnClickListener(this)
+        serviceIntent = Intent(this, MovementService::class.java)
     }
+
+    override fun onStart() {
+        super.onStart()
+        startService(serviceIntent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.clearButton -> {
+
+            }
+            R.id.exitButton -> {
+                // TODO: Stop service here
+                stopService(serviceIntent)
+                Log.d(localClassName, "Service successfully stopped.")
+                finish()
+            }
+        }
+    }
+
 }
---
+++ asg3/app/src/main/java/xyz/willnwalker/asg3/MovementService.kt
@@ -0,0 +1,15 @@
+package xyz.willnwalker.asg3
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MovementService: Service() {
+
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
